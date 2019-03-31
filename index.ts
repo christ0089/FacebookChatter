9a64d598990cb57f60f7c105e6aebe73d740dea2
Christian <christianalva96@gmail.com>
2019-03-30 20:53:10 -0600

    Messagges

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 
 
 const express = require('express');
-
 const request = require('request');
+const login = require("facebook-chat-api");
+
+
 
 const app = express();
 
@@ -11,25 +13,26 @@ app.get("", (req, res) => {
     res.status(200).send('<h1>Hello World</h1>');
 });
 
+
 app.get("/message", (req, res) => {
 
-    let token = "EAAFZC54cpsSkBAJniWOWSnGuPY2FXZCZA9VFMZAnylhNqLBuiZCUbJwdeNuZBJjolocdZCxMNQE8oUazZA6ZA4g87Tyxyqx9jDNj0P24aXmM9nk21zuMMgjz4D7F2ib5Y96tJpUzzXGzuqgLU6X6U1S6m1rvLfv3YJqNL5ia7n1b8PtPcpvBOF6CSMt0j8i0dlolWz4eQR1HbWAZDZD"
-    let id = "2212532525436748"
-    let casales_id = "1053686028"
-    var batchUrl = `https://graph.facebook.com`;
-    var r = request.post(batchUrl, function (error, response, body) {
-        if (error) { return console.log("error\n", error) };
-        console.log("successfull\n", body);
-    })
-    var form = r.form();
-    var recipient = "recipient=" + encodeURIComponent(JSON.stringify({ "id": id }));
-    var batchMessage = {
-        "method": "POST",
-        "relative_url": "v2.6/me/messages",
-        "body": recipient + "&" + "Hola"
-    };
-    form.append("access_token", token)
-    form.append("batch", JSON.stringify([batchMessage]));
+    login({email: "", password:""}, (err, api) => {
+        if(err) return console.error(err);
+        let id = "2212532525436748";
+        let casales_id = "1053686028"
+        var msg = {
+            body: "Hey!",
+          
+        }
+        api.sendMessage(msg, casales_id);
+    });
+
+    res.send("Hi :)");
+})
+
+app.get("/logout", (req, res) => {
+
+  
     res.send("Hi :)");
 })
 
---
+++ variables.js
@@ -0,0 +1,6 @@
+ const data = {
+    "email" : "christian-alva96@hotmail.com",
+    "pass" : "Armagedon12"
+}
+
+module.exports = data;
